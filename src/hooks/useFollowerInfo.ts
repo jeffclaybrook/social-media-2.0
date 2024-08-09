10d85da3f0af353ba1b8e34c6dd44cd53bf5faed
Jeff Claybrook <jeffclaybrook@Jeffs-MacBook-Pro.local>
2024-08-09 13:40:00 -0500

    Initial commit

---
+++ src/hooks/useFollowerInfo.ts
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query"
+import { FollowerInfo } from "@/lib/types"
+import kyInstance from "@/lib/ky"
+
+export default function useFollowerInfo(
+ userId: string,
+ initialState: FollowerInfo
+) {
+ const query = useQuery({
+  queryKey: ["follower-info", userId],
+  queryFn: () =>
+    kyInstance.get(`/api/users/${userId}/followers`).json<FollowerInfo>(),
+  initialData: initialState,
+  staleTime: Infinity
+ })
+
+ return query
+}
